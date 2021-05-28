48935524e67c05a6909847be1b48351c3968f879
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-05-28 13:59:41 -0500

    refactor code

--- util/bin/index.ts
+++ util/bin/index.ts
@@ -2,25 +2,50 @@
 
 import path from "path";
 import fs from "fs-extra";
+import glob from "glob";
 import { execSync } from "child_process";
 
 const [bin, file, dir] = process.argv;
 
-if (!dir) process.exit();
+if (!dir) {
+  console.error("missing directory");
+  process.exit(1);
+}
 
 const version = [0, 0, Date.now()].join(".");
 
 const root = path.resolve(dir);
 
+if (!fs.existsSync(root)) {
+  console.error(`not found "${root}"`);
+  process.exit(1);
+}
+
 const pckg = path.resolve(root, "package.json");
+
+if (!fs.existsSync(pckg)) {
+  console.error(`not found "${pckg}"`);
+  process.exit(1);
+}
 
 const data = fs.readJSONSync(pckg);
 
-fs.outputJSONSync(pckg, { ...data, version, private: true }, { spaces: 2 });
+console.log(`found package "${data.name}"`);
+
+/**
+ * Clear old packs
+ */
+const olds = glob.sync(`${root}/*.tgz`);
+
+if (olds.length) {
+  console.log("remove old packs");
+  olds.forEach((file) => fs.removeSync(file));
+}
 
 console.log(`set version "${version}"`);
 
-execSync("npm pack", { cwd: root, stdio: "inherit" });
+fs.outputJSONSync(pckg, { ...data, version, private: true }, { spaces: 2 });
 
+execSync("npm pack", { cwd: root, stdio: "inherit" });
 
 export = null;
