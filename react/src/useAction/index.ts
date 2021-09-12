bc00850fb4300dae6636f4719f860e8ebcf38e6f
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-09-12 12:19:24 -0500

    refactor useAction

--- react/src/useAction/index.ts
+++ react/src/useAction/index.ts
@@ -1,127 +1,136 @@
-import { useMemo } from "react";
+import React, { useMemo } from "react";
+
+function isFunc(val: any): val is (...args: any[]) => any {
+  return typeof val === "function";
+}
+
+export function createSync<T, R>(setState: T, reducer: R): Action<T, R>;
+export function createSync(setState: any, red: any) {
+  const res: any = { setState };
+
+  Object.entries(red).forEach(([key, val]) => {
+    if (!isFunc(val)) return;
+
+    res[key] = (...args: any[]) => {
+      setState((state: any) => val.call(null, state, ...args));
+      return res;
+    };
+  });
+
+  return res;
+}
+
+export function createAsync<T, R>(dispatch: T, reducer: R): AsyncAction<T, R>;
+export function createAsync(dispatch: any, red: any) {
+  const res: any = {};
+
+  Object.entries(red).forEach(([key, val]) => {
+    if (!isFunc(val)) return;
+
+    res[key] = (...args: any[]) => {
+      return val.call(null, dispatch, ...args);
+    };
+  });
+
+  return res;
+}
+
+export function createThunk<T, D, R>(
+  setState: T,
+  dispatch: D,
+  reducer: R
+): ThunkAction<T, D, R>;
+export function createThunk(setState: any, dispatch: any, red: any) {
+  const res: any = {};
+
+  Object.entries(red).forEach(([key, red]) => {
+    if (!isFunc(red)) return;
+    
+    res[key] = (...args: any[]) => {
+      const thunk = red(...args);
+    
+      return new Promise((res, rej) => {
+        setState((state: any) => {
+          const prom = thunk(state, dispatch);
+
+          if (prom.then) {
+            prom.then(res).catch(rej);
+          } else {
+            rej(new Error("thunk must return a promise"));
+          }
+
+          return state;
+        });
+      });
+    };
+  });
+
+  return res;
+}
+
+export const useThunk: CreateThunk = (setState, dispatch, reducer) => {
+  return React.useMemo(
+    () => createThunk(setState, dispatch, reducer),
+    [setState, dispatch, reducer]
+  );
+};
+
+export const useAsync: CreateAsync = (dispatch, reducer) => {
+  return React.useMemo(
+    () => createAsync(dispatch, reducer),
+    [dispatch, reducer]
+  );
+};
+
 /**
  * ok ... what you found here is a proposal for the implementation of an alternative
  * to redux and useReducer, also simplifying the API (or at least that's what I try),
  * its use is intended for small to medium applications, which is just a project to
  * test what you have learned about react.
  */
-
-/**
- * Inject action in thunks
- * @param action associative array sync reducer
- * @param reducer associative array async reducer
- */
-function createThunk(action: any, reducer: any): any {
-  return Object.fromEntries(
-    Object.entries(reducer).map(([key, val]) => {
-      if (typeof val === "function") {
-        /**
-         * wrap whith current actions
-         */
-        return [key, (...args: any[]) => val.call(null, action, ...args)];
-      } else if (typeof val === "object") {
-        /**
-         * Allow nested thunks
-         */
-        return [key, createThunk(action, val)];
-      }
-
-      return [key, null];
-    })
+export const useAction: CreateSync = (setState, reducer) => {
+  return React.useMemo(
+    () => createSync(setState, reducer),
+    [setState, reducer]
   );
-}
+};
 
-/**
- * Wrap action with setState
- *
- * Warning!!! this is experimental
- *
- */
-export function createAction<T, R>(
-  setState: SetState<T>,
-  reducer: R
-): Action<R, T>;
-export function createAction<T, R, A>(
-  setState: SetState<T>,
-  reducer: R,
-  thunk: A
-): Actions<T, R, A>;
-export function createAction(setState: any, reducer: any, thunk?: any): any {
-  const res: any = { setState };
-
-  res.setState = setState;
-
-  Object.keys(reducer).forEach((key) => {
-    const value = reducer[key];
-
-    const isFunc = typeof value === "function";
-    const isObj = typeof value === "object";
-
-    if (isFunc) {
-      res[key] = (...args: any[]) => {
-        setState((state: any) => value.call(null, state, ...args) || state);
-        return res;
-      };
-    } else if (isObj) {
-      res[key] = createAction(setState, value);
-    }
-  });
-
-  if (!thunk) {
-    return res;
-  }
-
-  const resultT = createThunk(res, thunk);
-
-  return [res, resultT];
-}
-
-export const useAction: Create = ((setState: any, reducer: any, thunk: any) => {
-  return useMemo(() => {
-    return createAction(setState, reducer, thunk);
-  }, [setState, reducer, thunk]);
-}) as any;
+export default useAction;
 
 /**
  * Types
  */
-type Create = typeof createAction;
+export type CreateThunk = typeof createThunk;
 
-export type Actions<T, R, A> = [Action<R, T>, Thunk<A, Action<R, T>>];
-
-export type Thunk<T, A> = A extends Action<infer R, infer S>
+export type ThunkAction<T, D, R> = T extends SetState<infer S>
   ? {
-      [K in keyof T]: T[K] extends (
-        action: Action<R, S>,
-        ...args: infer E
-      ) => infer F
-        ? (...args: E) => F
-        : T[K] extends {
-            [K: string]:
-              | ((action: Action<R, S>, ...args: any[]) => any)
-              | { [K: string]: (action: Action<R, S>, ...args: any[]) => any };
-          }
-        ? Thunk<T[K], A>
+      readonly [K in keyof R]: R[K] extends (
+        ...args: infer A
+      ) => (state: S, dispatch: D) => Promise<infer U>
+        ? (...args: A) => Promise<U>
         : never;
     }
   : never;
 
-export type Action<T, S> = {
-  readonly [K in keyof T]: T[K] extends (state: S, ...args: infer A) => S
-    ? (...args: A) => Action<T, S>
-    : T[K] extends {
-        [K: string]:
-          | ((state: S, ...args: any[]) => S)
-          | {
-              [K: string]:
-                | ((state: S, ...args: any[]) => S)
-                | { [K: string]: (state: S, ...args: any[]) => S };
-            };
-      }
-    ? Action<T[K], S>
+export type CreateAsync = typeof createAsync;
+
+export type AsyncAction<T, R> = {
+  readonly [K in keyof R]: R[K] extends (
+    setState: T,
+    ...args: infer A
+  ) => Promise<infer R>
+    ? (...args: A) => Promise<R>
     : never;
-} & {
-  setState: SetState<S>;
 };
 
-type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
+export type CreateSync = typeof createSync;
+
+export type Action<T, R> = T extends SetState<infer S>
+  ? {
+      readonly [K in keyof R]: R[K] extends (state: S, ...args: infer A) => S
+        ? (...args: A) => Action<T, R>
+        : never;
+    }
+  : never;
+
+export type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
