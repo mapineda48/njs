3c210485a34630a804fd6a5f2ed67139b24c1fc4
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-09-18 08:25:32 -0500

    add renderDev

--- react/src/render.ts
+++ react/src/render.ts
@@ -1,7 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom";
 
 export const root = document.getElementById("root") as HTMLDivElement;
 
 export function render(element: JSX.Element) {
   ReactDOM.render(element, root);
+}
+
+export function renderDev(element: () => JSX.Element) {
+  let container = root;
+
+  if (!container) {
+    container = document.createElement("div");
+    container.id = "root";
+    document.body.appendChild(container);
+  }
+
+  const Element = React.createElement(element, null, null);
+
+  ReactDOM.render(Element, container);
 }
