7c914355726a154d66d4bb117dbd3e9d60f182a4
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-10-18 08:20:49 -0500

    social add delay to send notification

--- social/lib/web-push/index.ts
+++ social/lib/web-push/index.ts
@@ -1,14 +1,31 @@
 import webPush from "web-push";
+import ms from "ms";
 
 import type { Store } from "../store";
 import type { Payload } from "./type";
 
 const subject = "mailto:example@yourdomain.org";
-
 let vapidDetails: VapidDetails = null as any;
+
+/**
+ * To avoid a flood of messages, there must be a 10 minute 
+ * difference between message and message.
+ */
+const timeLimit = ms("10m");
+let lastMessage = 0;
 
 export function prepareToSend(store: Store) {
   return async function sendNotify(payload: Payload) {
+    const timeMessage = Date.now();
+
+    if (lastMessage) {
+      const elapsed = timeMessage - lastMessage;
+
+      if (elapsed < timeLimit) {
+        return Promise.resolve();
+      }
+    }
+
     if (!vapidDetails) {
       const vapidKeys = await store.getVapidKeys();
 
@@ -22,6 +39,8 @@ export function prepareToSend(store: Store) {
     subs.forEach((sub) => {
       webPush.sendNotification(sub, data, { vapidDetails });
     });
+
+    lastMessage = timeMessage;
   };
 }
 
