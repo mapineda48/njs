321ccd604f62b90741158609a3897c4e24f8bab6
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-10-18 10:35:13 -0500

    personal minor change

--- personal/frontend/src/index.ts
+++ personal/frontend/src/index.ts
@@ -1,16 +1,16 @@
-const onErrorOld = window.onerror;
+// const onErrorOld = window.onerror;
 
-window.onerror = function (event, url, line, error) {
-  if (!url && !line && !error) {
-    // eslint-disable-next-line no-script-url
-    window.location.href = "javascript:location.reload(true)";
-    return;
-  }
+// window.onerror = function (event, url, line, error) {
+//   if (!url && !line && !error) {
+//     // eslint-disable-next-line no-script-url
+//     window.location.href = "javascript:location.reload(true)";
+//     return;
+//   }
 
-  if (onErrorOld) {
-    return onErrorOld.call(this, event, url, line, error);
-  }
-};
+//   if (onErrorOld) {
+//     return onErrorOld.call(this, event, url, line, error);
+//   }
+// };
 
 // eslint-disable-next-line import/first
 import "./web";
