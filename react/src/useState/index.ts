1a663a1ec2c18602476dc31855dada3aba622b77
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2022-01-30 15:28:41 +0000

    minor refactor

--- react/src/useState/index.ts
+++ react/src/useState/index.ts
@@ -1,48 +1,69 @@
 import React from "react";
 
 /**
- * Well, I'm still learning about how javascript engines work (V8), feel free to leave me a 
+ * Well, I'm still learning about how javascript engines work (V8), feel free to leave me a
  * comment if you notice any bad practices.
  */
+
+/**
+ * Keep state mount componente
+ */
+const mount = Symbol();
 
 /**
  * This function creates a closure, returns a React.useState wrapper, which won't update the state
  * if the component isn't mounting, not sure if I still have a memory leak, use with caution.
  */
-export function createSetStateAsync(): typeof React.useState {
-  let mount = false;
+export function ensureAction<T>(
+  reducer: T
+): T extends { [key: string]: (state: infer S, ...args: any[]) => any }
+  ? T extends { [key: string]: (state: S, ...args: any) => S }
+    ? (
+        initState: S | (() => S)
+      ) => [S, SetState<S>, { isMount: () => boolean } & Action<S, T>]
+    : never
+  : never;
+export function ensureAction(red: any): any {
+  return function useStateAction(val: any) {
+    const [state, setState] = React.useState(val);
 
-  return function useStateIfIsMount(val: any): any {
+    const action: any = React.useMemo(() => {
+      const res = Object.fromEntries(
+        Object.entries(red)
+          // Only functions
+          .filter(([, val]) => isFunc(val))
+          // use setState with actions
+          .map(([key, val]: any) => {
+            return [
+              key,
+              function (this: any, ...args: any[]) {
+                setState((state: any) => val.call(null, state, ...args));
+                return this;
+              },
+            ];
+          })
+      );
+
+      res[mount] = false;
+
+      res.isMount = function isMount() {
+        return this[mount];
+      };
+
+      return res;
+    }, [setState]);
+
     React.useEffect(() => {
-      mount = true;
+      action[mount] = true;
 
       return () => {
-        mount = false;
+        action[mount] = false;
       };
     });
 
-    const [state, setState] = React.useState(val);
-
-    const setStateIfIsMount = React.useCallback(
-      (val: any) => {
-        if (!mount) return;
-
-        setState(val);
-      },
-      [setState]
-    );
-
-    return [state, setStateIfIsMount];
-  } as any;
+    return [state, setState, action];
+  };
 }
-
-/**
- * Imagining a lite version of redux, and that each action has its own reducer
- * @param reducer reducer
- */
-export const initActionAsync: typeof initAction = (red) => {
-  return (initAction as any)(red, createSetStateAsync());
-};
 
 /**
  * Imagining a lite version of redux, and that each action has its own reducer
@@ -55,36 +76,30 @@ export function initAction<T>(
     ? (initState: S | (() => S)) => [S, SetState<S>, Action<S, T>]
     : never
   : never;
-export function initAction(red: any, useState = React.useState): any {
+export function initAction(red: any): any {
   return function useStateAction(val: any) {
-    const [state, setState] = useState(val);
+    const [state, setState] = React.useState(val);
 
-    const action = React.useMemo(() => createAction(setState, red), [setState]);
+    const action: any = React.useMemo(() => {
+      return Object.fromEntries(
+        Object.entries(action)
+          // Only functions
+          .filter(([, val]) => isFunc(val))
+          // use setState with actions
+          .map(([key, val]: any) => {
+            return [
+              key,
+              function (this: any, ...args: any[]) {
+                setState((state: any) => val.call(null, state, ...args));
+                return this;
+              },
+            ];
+          })
+      );
+    }, [setState]);
 
     return [state, setState, action];
   };
-}
-
-export function createAction<S, R>(
-  setState: SetState<S>,
-  reducer: R
-): Action<S, R>;
-export function createAction(setState: any, action: any) {
-  return Object.fromEntries(
-    Object.entries(action)
-      // Only functions
-      .filter(([, val]) => isFunc(val))
-      // use setState with actions
-      .map(([key, val]: any) => {
-        return [
-          key,
-          function (this: any, ...args: any[]) {
-            setState((state: any) => val.call(null, state, ...args));
-            return this;
-          },
-        ];
-      })
-  );
 }
 
 export function isFunc(val: any): val is (...args: any[]) => any {
