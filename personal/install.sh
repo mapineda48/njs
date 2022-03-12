7a089050fd9211a15c65a3769a0d2649c96c24a3
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2022-03-12 15:18:08 +0000

    personal add docker-compose.yml

---
+++ personal/install.sh
@@ -0,0 +1,8 @@
+apt update
+
+# puppeter packages depends on it
+apt install chromium 
+
+git config --global credential.helper store
+git config --global user.email "a.pinedavegamiguel@gmail.com"
+git config --global user.name "Miguel Pineda"
