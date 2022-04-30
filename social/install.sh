300974262952f4c5f7d00b50e0b6066afcbb8377
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2022-04-30 19:28:38 +0000

    minor change

--- social/install.sh
+++ social/install.sh
@@ -1,4 +1,6 @@
 # https://ngrok.com/download
 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok   
-              
+
+# https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry#authenticating-to-github-package-registry
+# npm login --scope=@mapineda48 --registry=https://npm.pkg.github.com
