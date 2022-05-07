b843272ec1893676c497b9e3c6d8a8adc5df5048
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2022-05-07 16:54:28 +0000

    refactor code

--- react/src/useState/index.ts
+++ react/src/useState/index.ts
@@ -1,10 +1,10 @@
 import React from "react";
+import { useStateSession } from "../useStateSession";
 
 /**
  * Well, I'm still learning about how javascript engines work (V8), feel free to leave me a
  * comment if you notice any bad practices.
  */
-
 /**
  * Keep state mount componente
  */
@@ -13,6 +13,7 @@ const mount = Symbol();
 /**
  * This function creates a closure, returns a React.useState wrapper, which won't update the state
  * if the component isn't mounting, not sure if I still have a memory leak, use with caution.
+ * @deprecated memory leaks
  */
 export function ensureAction<T>(
   reducer: T
@@ -69,14 +70,51 @@ export function ensureAction(red: any): any {
  * Imagining a lite version of redux, and that each action has its own reducer
  * @param reducer reducer
  */
-export function initAction<T>(
+export function initSession<T>(
+  reducer: T
+): T extends { [key: string]: (state: infer S, ...args: any[]) => any }
+  ? T extends { [key: string]: (state: S, ...args: any) => S }
+    ? (key: string, initState: S | (() => S)) => [S, SetState<S>, Action<S, T>]
+    : never
+  : never;
+export function initSession(red: any): any {
+  return function useStateAction(key: string, val: any) {
+    const [state, setState] = useStateSession(key, val);
+
+    const action: any = React.useMemo(() => {
+      return Object.fromEntries(
+        Object.entries(red)
+          // Only functions
+          .filter(([, val]) => isFunc(val))
+          // use setState with actions
+          .map(([key, val]: any) => {
+            return [
+              key,
+              function (this: any, ...args: any[]) {
+                setState((state: any) => val.call(null, state, ...args));
+                return this;
+              },
+            ];
+          })
+      );
+    }, [setState]);
+
+    return [state, setState, action];
+  };
+}
+
+/**
+ * Imagining a lite version of redux, and that each action has its own reducer
+ * @param reducer reducer
+ */
+export function initReducer<T>(
   reducer: T
 ): T extends { [key: string]: (state: infer S, ...args: any[]) => any }
   ? T extends { [key: string]: (state: S, ...args: any) => S }
     ? (initState: S | (() => S)) => [S, SetState<S>, Action<S, T>]
     : never
   : never;
-export function initAction(red: any): any {
+export function initReducer(red: any): any {
   return function useStateAction(val: any) {
     const [state, setState] = React.useState(val);
 
