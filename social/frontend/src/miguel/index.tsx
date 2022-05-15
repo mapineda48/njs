ae97daf5a168f847b70be92d703e9a41f3881a82
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2022-05-15 15:19:47 +0000

    make it better

--- social/frontend/src/miguel/index.tsx
+++ social/frontend/src/miguel/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 const container = document.getElementById("root");
 
