b7ba902152ed5f0221fc765fcbfd9b75e1f3ddfe
Miguel Pineda <miguel.pineda@imedicalservices.com>
2022-12-21 16:25:27 -0500

    make it better

--- src/Host/Img.tsx
+++ src/Host/Img.tsx
@@ -1,11 +1,14 @@
 import puppeteer from "../puppeteer";
 
 export function Img(props: Props) {
-  if (props.src) {
-    props.src = puppeteer.currentHost + props.src;
+  let src = props.src;
+
+  if (src) {
+    src = puppeteer.currentHost + props.src;
   }
+
   // eslint-disable-next-line jsx-a11y/alt-text
-  return <img {...props} />;
+  return <img {...props} src={src} />;
 }
 
 /**
